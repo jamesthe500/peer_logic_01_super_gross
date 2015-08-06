dd0e0e8eeb4370b3bc1909b5f2b377cfeef11bb3
jamesthe500 <jamesddaniels@gmail.com>
2015-08-06 14:27:51 -0500

    Achieves basic doneness

--- server/app.js
+++ server/app.js
@@ -1,13 +1,24 @@
 var express = require('express');
 var path = require('path');
 var app = express();
-
+var adjectives = require("./public/data/adjectives");
+var nouns = require("./public/data/nouns");
+var metaAdj = require("./public/data/metaAdj");
 var index = require('./routes/index');
 
 app.set('port', (process.env.PORT || 5000));
 
-app.get('/data', function(req, res){
-    /*res.json(people);*/
+
+app.get('/data/nouns', function(req, res){
+    res.json(nouns);
+});
+
+app.get('/data/metaAdj', function(req, res){
+    res.json(metaAdj);
+});
+
+app.get('/data/adjectives', function(req, res){
+    res.json(adjectives);
 });
 
 app.use('/', index);
